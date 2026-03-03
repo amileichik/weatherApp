2b22feb5d5eb64dafaba343510aa22e72dfff6b7
AMileychik <amileychik@gmail.com>
2026-03-02 21:21:08 -0500

    initial project

---
+++ Weather/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  Weather
+//
+//  Created by Александр Милейчик on 3/2/26.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
